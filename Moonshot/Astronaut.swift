dcdd01431d8baa6c7da2bdf8b78f30c84c0c0a67
Sadman Adib <sadmanadib67@gmail.com>
2022-06-27 13:43:15 +0600

    basics done

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Sadman Adib on 27/6/22.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
